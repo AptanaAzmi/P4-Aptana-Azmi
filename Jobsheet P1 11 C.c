4e4809b31730c20b284c3b62e89e9bc5ee7cc953
AptanaAzmi <64625905+AptanaAzmi@users.noreply.github.com>
2020-05-11 22:28:01 +0800

    Create Jobsheet P1 11 C.c
    Membuat program dengan header math.h

---
+++ Jobsheet P1 11 C.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int x=6;
+	int y=19;
+	float p,c,h;
+	c= y*10;
+	p= x+c;
+	h= sqrt (p);
+	printf("hasil dar akar x+y*10 dengan x nomor kelompok dan y noomr angkatan adlah %f",h);
+	return 0;
+}
