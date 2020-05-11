02ebc1da6fcf806fb7febbae9f748c2482938249
AptanaAzmi <64625905+AptanaAzmi@users.noreply.github.com>
2020-05-11 22:23:03 +0800

    Create Jobsheet P1 8 C.c
    Operating incerment dan decrement dengan niai statis

---
+++ Jobsheet P1 8 C.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 25;
+	int b = 5;
+	int c;
+	c--;
+
+	printf("nilai awal : a = %d, b=%d\n",a,b);
+	printf("ditanyakan : c = a / b adalah %d\n",c);
+	c--;
+	printf("maka nlai akhir c = a / b adalah %d",c);
+	
+	return 0;
+}
