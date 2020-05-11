0ecaef4d258f89f8f292f7242bb4fa4e568b58cd
AptanaAzmi <64625905+AptanaAzmi@users.noreply.github.com>
2020-05-11 22:21:46 +0800

    Create Jobsheet P1 7 C.c
    operator dengan nilai dinamis

---
+++ Jobsheet P1 7 C.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b, c;
+	printf("masukkan nilai a:"),
+	scanf("%i",&a);
+	printf("masukkan nilai b:");
+	scanf("%i",&b);
+	printf("hasil a / b : %i", c);
+}
