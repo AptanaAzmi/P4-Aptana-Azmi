83aea52e6c39a03a8431e2d625a984b658ce5631
AptanaAzmi <64625905+AptanaAzmi@users.noreply.github.com>
2020-05-12 13:36:20 +0800

    Create Jobsheet P2 16 C++.cpp
    Struktur perulangan label untuk deret bilangan

---
+++ Jobsheet P2 16 C++.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std; 
+int main ()
+{
+int i=199, a;
+cout<<"Masukkan angka : ";cin>>a;
+awal:
+a++;
+cout<<a<<",";
+if(i>=a)
+goto awal;
+else
+goto akhir;
+akhir:
+return 0;
+}
