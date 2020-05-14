f3c057675a973e5e55f16eb02060d5a7b02b1d7e
AptanaAzmi <64625905+AptanaAzmi@users.noreply.github.com>
2020-05-14 11:24:37 +0800

    Create Jobsheet P4 1 C.c
    Menampilkan array satu dimensi

---
+++ Jobsheet P4 1 C.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+main()
+{
+	int my_array[5] = { 5,2,0,9,1};
+	printf("Nama : Aptana Azmi\n");
+	printf("NIM  : FIB019025\n");
+	printf("Kelompok: 6\n");
+	printf("Data ke-1 = %d",my_array[0]);
+	printf("\nData ke-2 = %d",my_array[1]);
+	printf("\nData ke-3 = %d",my_array[2]);
+	printf("\nData ke-4 = %d",my_array[3]);
+	printf("\nData ke-5 = %d",my_array[4]);
+}
